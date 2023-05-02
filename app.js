d6ed14e01582a05ba48c4c56dd6a1a7888e64f11
sreyaarc <sreya66nambiar@gmail.com>
2023-05-02 21:26:10 +0530

    add tasks to the to-do list

--- app.js
+++ app.js
@@ -3,9 +3,29 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}));
 
+app.set('view engine', 'ejs');  
+
+var tasks = [];
+
 app.get("/", (req, res) => {
-    res.send("<h1>hey there!!</h1>");
+    let today = new Date();
+
+    let options = {
+        day : "numeric",
+        month: "long",
+        year: "numeric"
+    };
+    
+    var day = today.toLocaleDateString("en-US", options);
+    res.render("list", {typeOfDay: day, tasks: tasks});
 });
+
+app.post("/", (req, res) => {
+    // res.send(`<h2>${req.body.newTask}</h2>`)
+    var task = req.body.newTask;
+    tasks.push(task);
+    res.redirect("/");
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
