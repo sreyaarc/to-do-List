435304902bd37096058e2cd7068225ab154d9508
sreyaarc <sreya66nambiar@gmail.com>
2023-05-04 08:58:21 +0530

    modify app.js for adding tasks

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 import express from "express";
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.set('view engine', 'ejs');  
 
-var tasks = [];
+let tasks = [];
+
+app.use(express.static(path.join(__dirname , "/public")));
 
 app.get("/", (req, res) => {
     let today = new Date();
-
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
     let options = {
         day : "numeric",
         month: "long",
         year: "numeric"
     };
     
-    var day = today.toLocaleDateString("en-US", options);
-    res.render("list", {typeOfDay: day, tasks: tasks});
+    let generalDate = today.toLocaleDateString("en-US", options);
+    let typeOfDay = `${days[today.getDay()]}, ${months[today.getMonth()]} ${today.getDate()}`
+
+    res.render("list", {
+        generalDate: generalDate, 
+        currentTime: today.toLocaleTimeString(),
+        typeOfDay: typeOfDay,
+        tasks: tasks});
 });
 
+app.use(express.urlencoded({extended: true}));
 app.post("/", (req, res) => {
     // res.send(`<h2>${req.body.newTask}</h2>`)
-    var task = req.body.newTask;
+    let task = req.body.inputTask;
     tasks.push(task);
+
     res.redirect("/");
 })
 
